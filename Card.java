88372d9901e22eeddf4c8ae96fb9446d7c4c4c0f
Samuel Hug <samuel.b.hug@gmail.com>
2013-05-22 14:34:09 -0700

    Card symbols now scale with the cards

--- Card.java
+++ Card.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
@@ -13,133 +14,184 @@ import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.border.Border;
 
-
 public class Card extends JComponent {
-	
+
 	private final Color CARD_BORDER_COLOR = Color.green;
 	private final Color CARD_FACE_COLOR = Color.white;
 	private final Color CARD_BACK_COLOR = Color.cyan;
-	private final Color TEXT_COLOR = Color.red;
-	
-	/* Creates borders for the card cell. One normal,
-	 * and one to display with the mouse hovers over it. */
+	private final Color SYMBOL_COLOR = Color.red;
+
+	/*
+	 * Creates borders for the card cell. One normal, and one to display when
+	 * the mouse hovers over it.
+	 */
 	private final Border BORDER_DEFAULT = BorderFactory.createLineBorder(Color.black);
 	private final Border BORDER_HOVER = BorderFactory.createLineBorder(Color.yellow);
-	
+
+	private final Font SYMBOL_FONT = new Font("Arial", Font.PLAIN, 1);
+	private final double SYMBOL_FONT_SIZE_FACTOR = 0.6;
+
 	// Playing cards are not square. This is a ratio of the length of sides `WIDTH / HEIGHT`
-	private final double ASPECT_RATIO = (double)5/7;
-	
-	// This is not a pixel count its a percentage of the total vertical height. 
+	private final double ASPECT_RATIO = (double) 5 / 7;
+
+	// This is not a pixel count its a percentage of the total vertical height.
 	private final double PADDING = 0.1;
-	
-	public enum CardState {
-		FACE_DOWN,  // The card is face down, only the patterned side is visible.
-		FACE_UP,    // The card is face up, the front side is visible.
-		OUT_OF_PLAY, // The card is out of play, it has already been matched.
-		MID_FLIP_UP,
-		MID_FLIP_DOWN,
-	}
-	
+
+	private final char symbol;
+
 	private CardState cardState;
 	private Rectangle2D cardRect;
-	
+
 	private List<ClickListener> listeners = new ArrayList<ClickListener>();
-	
-	public Card() {
-		
+
+	/**
+	 * Constructs a card object.
+	 * 
+	 * @param symbol
+	 *            The symbol to display on the face of the card.
+	 */
+	public Card(final char symbol) {
+		this.symbol = symbol;
+
 		// Set the initial state of the card
 		setCardState(CardState.FACE_DOWN);
 
 		this.setBorder(BORDER_DEFAULT);
-		
+
 		this.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				// If the mouse is inside the card rectangle
+				/*
+				 * If the mouse clicked while it was over the card, notify all
+				 * of the ClickListeners
+				 */
 				if (cardRect.contains(e.getPoint())) {
 					for (ClickListener listener : listeners) {
-						listener.cardClicked((Card)e.getComponent());
+						listener.cardClicked((Card) e.getComponent());
 					}
 				}
 			}
+
 			@Override
-			public void mousePressed(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {
+			}
+
 			@Override
-			public void mouseReleased(MouseEvent e) {}
+			public void mouseReleased(MouseEvent e) {
+			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
+				/*
+				 * When the mouse moves into the cards cell, change the border color.
+				 */
 				setBorder(BORDER_HOVER);
 				repaint();
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
+				/*
+				 * When the mouse moves out of the cards cell, change the border
+				 * color back to normal.
+				 */
 				setBorder(BORDER_DEFAULT);
 				repaint();
 			}
 		});
 	}
-	
+
 	/**
 	 * Paint the card and its surrounding border to the given graphics object.
 	 * 
 	 * NOTE: The given width and height correspond to the size of the grid cell,
-	 *       NOT the card size. The size of the card is calculated as the largest
-	 *       rectangle, where the ratio of it's sides is `ASPECT_RATIO`, that will
-	 *       fit in the grid cell.
+	 * NOT the card size. The size of the card is calculated as the largest
+	 * rectangle, where the ratio of it's sides is `ASPECT_RATIO`, that will fit
+	 * in the grid cell.
 	 * 
-	 * @param g The graphics object to paint to.
-	 * @param width The width of the containing cell.
-	 * @param height The height of the containing cell.
+	 * @param g
+	 *            The graphics object to paint to.
+	 * @param width
+	 *            The width of the containing cell.
+	 * @param height
+	 *            The height of the containing cell.
 	 */
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		final Dimension size = this.getSize();
-		
-		final Graphics2D g2d = (Graphics2D)g.create();
-	    try {
+
+		final Graphics2D g2d = (Graphics2D) g.create();
+		try {
+			final Dimension size = this.getSize();
+			final String symbol = Character.toString(this.symbol);
+
+			// Calculate the size of the card given the size of the cell.
 			cardRect = calculateCardRect(size.getWidth(), size.getHeight());
-			
-			//g2d.translate(cardRect.getX(), cardRect.getY());
-			
+
 			if (cardState == CardState.FACE_DOWN) {
 				g2d.setColor(CARD_BACK_COLOR);
 				g2d.fill(cardRect);
-				
+
 				g2d.setColor(CARD_BORDER_COLOR);
 				g2d.draw(cardRect);
 			} else {
 				g2d.setColor(CARD_FACE_COLOR);
 				g2d.fill(cardRect);
-				
+
 				g2d.setColor(CARD_BORDER_COLOR);
 				g2d.draw(cardRect);
-				
-				g2d.setColor(TEXT_COLOR);
-				g2d.translate(cardRect.getX(), cardRect.getY());
-				g2d.drawString("A", (float)cardRect.getWidth()/2, (float)cardRect.getHeight()/2);
+
+				// Draw the symbol in the center of the card
+				g2d.setColor(SYMBOL_COLOR);
+
+				final Font font = SYMBOL_FONT.deriveFont(
+						(float) (cardRect.getHeight() * SYMBOL_FONT_SIZE_FACTOR));
+				g2d.setFont(font);
+
+				final Rectangle2D symbolBounds = font.getStringBounds(symbol,
+						g2d.getFontRenderContext());
+				g2d.translate(
+						cardRect.getCenterX() - symbolBounds.getCenterX(),
+						cardRect.getCenterY() - symbolBounds.getCenterY());
+
+				g2d.drawString(symbol, 0, 0);
 			}
-	    } finally {
-	         g2d.dispose();
-	    }
+		} finally {
+			g2d.dispose();
+		}
 	}
-	
+
 	/**
-	 * Calculates the maximum size of the card with the correct aspect-ratio within
-	 * the given bounds.
+	 * Flips the card. If it's face-down, make it face-up. If its face-up, make
+	 * it face-down.
+	 */
+	public void flip() {
+		if (getCardState() == Card.CardState.FACE_DOWN) {
+			setCardState(Card.CardState.FACE_UP);
+		} else if (getCardState() == Card.CardState.FACE_UP) {
+			setCardState(Card.CardState.FACE_DOWN);
+		}
+	}
+
+	/**
+	 * Calculates the maximum size of the card with the correct aspect-ratio
+	 * within the given bounds.
 	 * 
-	 * @param boundWidth The maximum width.
-	 * @param boundHeight The maximum height
+	 * @param boundWidth
+	 *            The maximum width.
+	 * @param boundHeight
+	 *            The maximum height
 	 * @return A rectangle
 	 */
 	public Rectangle2D calculateCardRect(double boundWidth, double boundHeight) {
-		
+
 		// Calculate the size of the card
-		double cardHeight = boundHeight - boundHeight*PADDING;
-		double cardWidth = Math.min(boundWidth - boundWidth*PADDING, cardHeight * ASPECT_RATIO);
+		double cardHeight = boundHeight - boundHeight * PADDING;
+		double cardWidth = Math.min(boundWidth - boundWidth * PADDING,
+				cardHeight * ASPECT_RATIO);
 		cardHeight = Math.min(cardHeight, boundWidth / ASPECT_RATIO);
-		
-		return new Rectangle2D.Double((boundWidth - cardWidth) / 2, (boundHeight - cardHeight) / 2, cardWidth, cardHeight);
+
+		return new Rectangle2D.Double((boundWidth - cardWidth) / 2,
+				(boundHeight - cardHeight) / 2, cardWidth, cardHeight);
 	}
 
 	/**
@@ -150,18 +202,31 @@ public class Card extends JComponent {
 	}
 
 	/**
-	 * @param cardState the card state to set
+	 * @param cardState
+	 *            the card state to set
 	 */
 	public void setCardState(CardState cardState) {
 		this.cardState = cardState;
 		repaint();
 	}
-	
+
+	/**
+	 * Adds a ClickListener to this Card object.
+	 * 
+	 * @param listener
+	 *            The ClickListener to add.
+	 */
 	public void addClickListener(ClickListener listener) {
 		listeners.add(listener);
 	}
-	
+
 	public interface ClickListener {
 		public void cardClicked(Card card);
+	}
+
+	public enum CardState {
+		FACE_DOWN, // The card is face down, only the patterned side is visible.
+		FACE_UP, // The card is face up, the front side is visible.
+		OUT_OF_PLAY, // The card is out of play, it has already been matched.
 	}
 }
--- MemoryGame.java
+++ MemoryGame.java
@@ -1,58 +1,83 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.util.Random;
 
 public class MemoryGame extends JApplet {
-	
+
 	private final int GRID_WIDTH = 4; // The number of cells wide the grid is.
 	private final int GRID_HEIGHT = 4; // The number of cells tall the grid is.
-	
+
+	private final String SYMBOL_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 	// The background color for the playing area
 	private final Color BACKGROUND_COLOR = Color.gray;
-	
+
 	// The total number of cards
 	private final int N_CARDS = GRID_WIDTH * GRID_HEIGHT;
-	
+
+	private final Random rand = new Random();
+
 	CardClickListener cardClickListener = new CardClickListener();
-	
-    /**
-     * Construct the window
-     */
-    public void init() {
-        // Set the background color.
-        getContentPane().setBackground(BACKGROUND_COLOR);
-        
-        // Construct a grid layout for the cards
-        GridLayout gridLayout = new GridLayout(GRID_WIDTH, GRID_HEIGHT);
-        this.setLayout(gridLayout);
-        
-        // Create the cards and put them in the grid layout
-        // TODO: Need to ensure the cards are created in pairs and the deck is shuffled.
-        for (int i=0; i<N_CARDS; i++) {
-        	
-        	// Create a new card
-        	final Card card = new Card();
-        	
-        	// Add our click listener to it
-        	card.addClickListener(cardClickListener);
-        	
-        	// Place the card in the grid layout
-        	this.add(card);
-        }
-    }
-    
-    private class CardClickListener implements Card.ClickListener {
+
+	/**
+	 * Construct the window
+	 */
+	public void init() {
+		// Set the background color.
+		getContentPane().setBackground(BACKGROUND_COLOR);
+
+		// Construct a grid layout for the cards
+		GridLayout gridLayout = new GridLayout(GRID_WIDTH, GRID_HEIGHT);
+		this.setLayout(gridLayout);
+
+		// Array to temporarily hold the cards.
+		Card cards[] = new Card[N_CARDS];
+
+		// Create the cards and put them in the card array
+		for (int i = 0; i < N_CARDS; i += 2) {
+
+			// Get a random symbol
+			final char symbol = getRandomSymbol();
+
+			// Create two cards with the same symbol
+			final Card card1 = new Card(symbol);
+			final Card card2 = new Card(symbol);
+
+			// Add our click listener to them
+			card1.addClickListener(cardClickListener);
+			card2.addClickListener(cardClickListener);
+
+			// Place the cards in the card array
+			cards[i] = card1;
+			cards[i + 1] = card2;
+		}
+
+		// TODO: Shuffle the card array
+
+		// Place the shuffled cards in the grid layout
+		for (int i = 0; i < N_CARDS; i++) {
+			this.add(cards[i]);
+		}
+	}
+
+	/**
+	 * Picks a random character from SYMBOL_ALPHABET and returns it.
+	 * 
+	 * @return The random character.
+	 */
+	private char getRandomSymbol() {
+		final int length = SYMBOL_ALPHABET.length();
+		return SYMBOL_ALPHABET.charAt(rand.nextInt(length));
+	}
+
+	private class CardClickListener implements Card.ClickListener {
 		@Override
 		public void cardClicked(Card card) {
-			
+
 			System.out.println("Card clicked!");
-			
-			if (card.getCardState() == Card.CardState.FACE_DOWN) {
-	    		card.setCardState(Card.CardState.FACE_UP);
-	    	}
-	    	else if (card.getCardState() == Card.CardState.FACE_UP) {
-	    		card.setCardState(Card.CardState.FACE_DOWN);
-	    	}
+
+			// Flip the card
+			card.flip();
 		}
-    }
+	}
 }
